f656396f03f254cdddd857b50f9f3be99c689cda
mijeongk <rkdalwjd17@gmail.com>
2023-09-04 17:31:52 +0900

    FileUpload

---
+++ FileUploadEx01/src/main/java/kr/kdata/upload/vo/FileVO.java
@@ -0,0 +1,16 @@
+package kr.kdata.upload.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FileVO {
+	
+	private String uuid; // 유일한 이름을 만들어 중복이름 
+	private String fileName; // 실제 파일이름
+	private String contentType; // 파일의 종류
+
+}
